bdda2db446f9f1be8559493bddd05f51954fae63
rlsplep <ignatovanasta152@gmail.com>
2026-05-13 10:33:31 +0300

    вытянула текст через value, начала джс

---
+++ script.js
@@ -0,0 +1,9 @@
+const AddBtn = document.getElementById('add-btn')
+
+const Input = document.getElementById('task-input')
+
+AddBtn.addEventListener('click', () => {
+    addbut = Input.value
+    console.log(addbut)
+
+})
