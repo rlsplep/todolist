ae16829e331022cc3d11953790955bba645497dd
rlsplep <ignatovanasta152@gmail.com>
2026-05-14 09:25:20 +0300

    добавила кнопку delete,разобралась с функциями

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const AddBtn = document.getElementById('add-btn')
+const AddBtn = document.getElementById('add-btn');
 
-const Input = document.getElementById('task-input')
+const Input = document.getElementById('task-input');
+const TaskList = document.getElementById('task-list');
+const EmptyMsg = document.getElementById('empty-msg');
+
 
 AddBtn.addEventListener('click', () => {
-    addbut = Input.value
-    console.log(addbut)
+    const addbut = Input.value;
+    if (addbut !== '') {
+        EmptyMsg.style.display = 'none';
+    }
 
+    const li = document.createElement('li');
+    li.className = "task-item";
+    li.innerHTML = `          <label class="checkbox-container">
+            <input type="checkbox" class="task-checkbox" />
+            <span class="custom-checkbox"></span>
+          </label>
+
+          <div class="task-plate">
+            <span class="task-text">${addbut}</span>
+
+            <div class="task-actions">
+              <button class="edit-btn">EDIT</button>
+              <button class="dlt-btn">DELETE</button>
+            </div>
+          </div>`;
+    TaskList.appendChild(li);
+    const dltBtn = li.querySelector('.dlt-btn');
+    dltBtn.addEventListener('click', () => {
+        li.remove();
+    })
+    Input.value = '';
 })
